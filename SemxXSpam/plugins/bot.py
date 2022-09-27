22ec5b7084119b6d16e9aaccb3f93d6a5d3cadf2
promaxbot1 <114501770+promaxbot1@users.noreply.github.com>
2022-09-27 20:49:24 +0530

    Update bot.py

--- SemxXSpam/plugins/bot.py
+++ SemxXSpam/plugins/bot.py
@@ -22,7 +22,7 @@ from datetime import datetime
 async def ping(e):
     if e.sender_id in SUDO_USERS:
         start = datetime.now()
-        event = await e.reply("𝙎𝘼𝘽𝙃𝘼𝙍 𝙍𝙃𝘼𝙆 𝙉𝘼..𝙂𝘼𝙉𝘿𝙐", parse_mode=None, link_preview=None )
+        event = await e.reply("𝙎𝘼𝘽𝙃𝘼𝙍 𝙍𝙃𝘼𝙆 𝙉𝘼...𝙂𝘼𝙉𝘿𝙐", parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
         await event.edit(f"👅👅 🇺𝗥𝗔𝗡𝗜𝗨𝗠 ✘ 🇸𝗣𝗔𝗠 🤖!\n✘`{ms}` ms\n 𝗖𝗛𝗨𝗣𝗣 𝗥𝗘𝗛𝗔𝗡𝗔 𝗠𝗔𝗗𝗘𝗥𝗖𝗛𝗢𝗗")
